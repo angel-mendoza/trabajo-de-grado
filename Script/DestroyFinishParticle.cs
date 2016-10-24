1ea4b63108688d201efd4ae2f247ba7fc63d9ec9
Angel Mendoza <mendozaangel003@gmail.com>
2016-10-23 20:06:26 -0400

    enemigo funcional y se resolvio el problema del movimiento de los personajes

--- Script/DestroyFinishParticle.cs
+++ Script/DestroyFinishParticle.cs
@@ -5,18 +5,23 @@ public class DestroyFinishParticle : MonoBehaviour {
 
     private ParticleSystem EsteSistemaDeParticulas;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         EsteSistemaDeParticulas = GetComponent<ParticleSystem>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update() {
         if (EsteSistemaDeParticulas.isPlaying)
         {
             return;
         }
 
         Destroy(gameObject);
-	}
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
 }
---
+++ Script/EnemyController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+    public float velocidadMovimiento;         //movespeed
+    public bool mueveteDerecha;               //moveRigth
+
+    public Transform checkDeCaminar;           //wallCheck
+    public float radioDelCheckDeCaminar;       // walldCheckRadius
+    public LayerMask estaCaminando;            //whatIsWall
+    private bool tropesar;                     //hittingWall
+
+    private bool atCaida;                      //atEdge
+    public Transform caidaCkeck;                //edgeCheck
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        tropesar = Physics2D.OverlapCircle(checkDeCaminar.position, radioDelCheckDeCaminar, estaCaminando);
+
+        //atCaida = Physics2D.OverlapCircle(caidaCkeck.position, radioDelCheckDeCaminar, estaCaminando);
+        atCaida = Physics2D.OverlapCircle(caidaCkeck.position, radioDelCheckDeCaminar, estaCaminando);
+        if (tropesar || !atCaida)
+        {
+            mueveteDerecha = !mueveteDerecha;
+        }
+
+
+        if (mueveteDerecha)
+        {
+            transform.localScale = new Vector3(1, 1, 1f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(velocidadMovimiento, GetComponent<Rigidbody2D>().velocity.y);
+        }else
+        {
+            transform.localScale = new Vector3(-1, 1, 1f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-velocidadMovimiento, GetComponent<Rigidbody2D>().velocity.y);
+        }
+	}
+}

8a034cd87a35fc475d11d3f56a306a7c545d01c9
Angel Mendoza <mendozaangel003@gmail.com>
2016-11-05 13:04:37 -0430

    el enemigo dispara al entrar al rango

---
+++ Script/EnemyProyectilController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyProyectilController : MonoBehaviour {
+    public float velocidad;
+
+    public EnemyController enemigo;
+
+    public int daño;
+
+    // Use this for initialization
+    void Start()
+    {
+        enemigo = FindObjectOfType<EnemyController>();
+
+      if (enemigo.transform.localScale.x < 0)
+        {
+            velocidad = -velocidad;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(velocidad, GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            HealthManager.HurtPlayer(daño);
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ Script/ShootAtPlayerInRange.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootAtPlayerInRange : MonoBehaviour {
+
+    public float rangoDelJugador;
+    public GameObject proyectilEnemigo;
+    public PlayerController player;
+    public Transform puntoDeLanzamiento;
+    public float tiempoParaDisparar;
+    private float contadorDeDisparo;
+
+	// Use this for initialization
+	void Start () {
+
+        player = FindObjectOfType<PlayerController>();
+        contadorDeDisparo = tiempoParaDisparar;
+;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Debug.DrawLine(new Vector3(transform.position.x + rangoDelJugador, transform.position.y, transform.position.z), new Vector3(transform.position.x - rangoDelJugador, transform.position.y, transform.position.z));
+        contadorDeDisparo -= Time.deltaTime;
+        if (transform.localScale.x > 0 && player.transform.position.x > transform.position.x && player.transform.position.x < transform.position.x + rangoDelJugador && contadorDeDisparo < 0)
+        {
+            Instantiate(proyectilEnemigo, puntoDeLanzamiento.position, puntoDeLanzamiento.rotation);
+            contadorDeDisparo = tiempoParaDisparar;
+
+        }
+
+        if (transform.localScale.x < 0 && player.transform.position.x < transform.position.x && player.transform.position.x > transform.position.x - rangoDelJugador && contadorDeDisparo < 0)
+        {
+            Instantiate(proyectilEnemigo, puntoDeLanzamiento.position, puntoDeLanzamiento.rotation);
+            contadorDeDisparo = tiempoParaDisparar;
+
+        }
+
+    }
+
+
+}

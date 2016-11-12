2952d520585dc9d22f8f66f095473b4275c1a53d
Angel Mendoza <mendozaangel003@gmail.com>
2016-11-12 09:38:26 -0430

    helice cortante

---
+++ Script/AspaController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AspaController : MonoBehaviour {
+    public float velocidadDeRotacion;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Rigidbody2D>().angularVelocity = velocidadDeRotacion;
+	}
+}

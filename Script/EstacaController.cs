db0789118c2b03e89d208ef48eedbf39d34fa7a4
Angel Mendoza <mendozaangel003@gmail.com>
2016-11-12 20:34:08 -0430

    estalagmitas

---
+++ Script/EstacaController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EstacaController : MonoBehaviour {
+    public GameObject estaca;
+    
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.name == "Player")
+        {
+            //GetComponent<Rigidbody2D>().gravityScale = 2;
+            estaca.GetComponent<Rigidbody2D>().gravityScale = 2;
+        }
+    }
+}
--- Script/HurtPlayerOnContact.cs
+++ Script/HurtPlayerOnContact.cs
@@ -33,5 +33,6 @@ public class HurtPlayerOnContact : MonoBehaviour {
                 player.derriboDerecha = false;
             }
         }
+
     }
 }
